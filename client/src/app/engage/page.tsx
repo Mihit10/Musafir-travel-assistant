24f69d039d224a38bca1043837454aa69158beef
Ria Talsania <riatalsania0510@gmail.com>
2025-04-12 18:22:15 +0530

    community page

---
+++ client/src/app/engage/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+// page.tsx
+import React from 'react';
+import CommunityPage from './community';
+
+const Page = () => {
+  return (
+    <div>
+      <CommunityPage />
+    </div>
+  );
+};
+
+export default Page;
