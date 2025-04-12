3c40f8df4a4e309d6d64230bc385e5880ac1d765
Ria Talsania <riatalsania0510@gmail.com>
2025-04-12 22:24:01 +0530

    benito

---
+++ client/src/app/info/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+// page.tsx
+import React from 'react';
+import BenitoGrid from './detail';
+
+const Page = () => {
+  return (
+    <div>
+      <BenitoGrid/>
+    </div>
+  );
+};
+
+export default Page;
